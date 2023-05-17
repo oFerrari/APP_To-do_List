079a610d500dcb47e59b98ed6cde84cc110d2ef0
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-05-17 16:20:22 -0300

    💄 Finalizando a estilização dos Cards

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,32 +1,45 @@
 import { Checkbox, Grid, IconButton, Typography } from "@mui/material";
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteForeverSharp';
+import CheckButton from "../CheckButton";
+import { Task } from "../../Types";
 
-export function CardList() {
+export function CardList(tasks: Task) {
+    
     return (
         <>
-            <Grid container wrap="nowrap" spacing={2} sx={{ paddingTop: '20px' }}>
-                <Grid container wrap="nowrap" spacing={2} alignItems="center">
-                    <Grid item>
-                        <Checkbox color="primary" />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Typography>tarefa</Typography>
-                    </Grid>
-                    <Grid item xs={9}>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.,
-                        Dolores ex quo hic quibusdam impedit aspernatur veritatis
-                        perspiciatis, temporibus, quidem consectetur eligendi
-                        similique suscipit tempora magnam natus, totam nisi quasi sed.
-                    </Grid>
-                </Grid>
+            <Grid container wrap="nowrap" spacing={2} sx={{ 
+                padding: '20px',
+                background:'#272727',
+                borderRadius:'10px'
+                }}>
+                <Grid container wrap="nowrap" spacing={4} alignItems="center">
 
-                <Grid item xs={1}>
-                    <Grid>
+                    <Grid item xs={1}>
+                        <CheckButton label="" />
+                    </Grid>
+
+                    <Grid item xs={10}>
+                        <Typography variant="body2" noWrap>
+
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.,
+                            Dolores ex quo hic quibusdam impedit aspernatur veritatis
+                            perspiciatis, temporibus, quidem consectetur eligendi
+                            similique suscipit tempora magnam natus, totam nisi quasi sed.
+
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={1}>
+                    
                         <IconButton aria-label="delete" size="large">
                             <DeleteOutlinedIcon />
                         </IconButton>
-                    </Grid>
+                    
                 </Grid>
+
+                </Grid>
+
+                
             </Grid>
         </>
     )
---
+++ src/components/CheckButton/index.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+
+interface CheckButtonProps {
+  label: string;
+}
+
+const CheckButton: React.FC<CheckButtonProps> = ({ label }) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleCheck = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <div onClick={handleCheck} style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+      <div style={{ width: '20px', height: '20px', borderRadius: '50%', border: '1px solid #42A5F5', marginRight: '8px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        {checked && <AiOutlineCheckCircle style={{ color: '#42A5F5', fontSize: '18px' }} />}
+      </div>
+      <span>{label}</span>
+    </div>
+  );
+};
+
+export default CheckButton;
